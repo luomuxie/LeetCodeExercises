8f990dc84e08a610b79a497471267ca97d6263f4
luomuxie <835657341@qq.com>
2019-06-18 18:25:11 +0800

    Update 67. AddBinary.cs

--- Math/67. AddBinary.cs
+++ Math/67. AddBinary.cs
@@ -1,16 +1,22 @@
+/* 
+* @Author: XIEXIAN  
+* @Date: 2019-06-18 15:17:26  
+ * @Last Modified by: XIEXIAN
+ * @Last Modified time: 2019-06-18 15:20:41
+*/
 using System;
 using System.Text;
 namespace LeetCodeExercises
 {
     public class AddBinary
     {
-        //解法一：不可，没通过
+        //解法一：不可，没通过=>这个方法，不能处理大数据，只能在int范围内处理
         public String addBinary01(String a, String b) {
             
-            return Convert.ToString((Convert.ToInt32(a,2)+Convert.ToInt32(b,2)),2);//这个方法，不能处理大数据，只能在int范围内处理
+            return Convert.ToString((Convert.ToInt32(a,2)+Convert.ToInt32(b,2)),2);
         }
 
-        //解法二：通过
+        //解法二：通过 104 ms	23.2 MB	csharp ,不知道为什么同样的方法，跑在java上又会快很多
         public String addBinary02(String a, String b) {
             
             StringBuilder sb = new StringBuilder();
