cbb69ab611b99bdc166099891112391933af6106
luomuxie <835657341@qq.com>
2019-06-25 16:08:06 +0800

    Update 69. Sqrt(x).cs

--- Math/69. Sqrt(x).cs
+++ Math/69. Sqrt(x).cs
@@ -22,8 +22,8 @@ namespace LeetCodeExercises
             {
                 int mid = start+(end-start) /2;
                 if(mid == x/mid) return mid;
-                else if(mid<x/mid) start = mid;
-                else end = mid;
+                else if(mid<x/mid) start = mid+1;
+                else end = mid+1;
             }            
             return end;
         }
