b5231acab8b7090b05750c455025869db3cad107
luomuxie <835657341@qq.com>
2019-07-15 14:49:02 +0800

    Update 144. BinaryTreePreorderTraversal.cs
    提取Tree数据结构

--- Tree/144. BinaryTreePreorderTraversal.cs
+++ Tree/144. BinaryTreePreorderTraversal.cs
@@ -8,14 +8,6 @@ using System.Collections.Generic;
 namespace LeetCodeExercises
 {
 
-  //Definition for a binary tree node.
-  public class TreeNode {
-      public int val;
-      public TreeNode left;
-      public TreeNode right;
-      public TreeNode(int x) { val = x; }
-  }
-
  /*
     需求：返回树的先根遍历
   */
